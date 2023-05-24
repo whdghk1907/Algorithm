c224e5be8e0cba559d3ec9a62909194d74a73e21
CHOIYEJUN <dpwns108@gmail.com>
2023-05-24 19:46:38 +0900

    fix($Step1.Average): 백준1546번 문제 풀이
    구분 : 1차원 배열
    
    메모리 : 14372kb
    시간 : 124ms
    
    시간복잡도 : O(1)
    공긴복잡도 : O(1)
    
    Closes 없음

--- src/main/java/oneDimentionArrey/Setp1/Average/Average.java
+++ src/main/java/oneDimentionArrey/Setp1/Average/Average.java
@@ -4,30 +4,29 @@ import java.io.BufferedReader;
 import java.util.Arrays;
 
 public class Average {
-
     public static void main(String[] args) throws Exception {
-
         BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+
+        int numberCount = Integer.parseInt(br.readLine());
         String arr[] = br.readLine().split(" ");
-        System.out.println(arr);
-        double arrayInt[] = new double[N];
+
+        double arrayInt[] = new double[numberCount];
+
         double max = 0;
         double sum = 0;
 
-        for(int i=0; i<N; i++){
+        for(int i = 0; i < numberCount; i++){
             arrayInt[i] = Integer.parseInt(arr[i]);
+
             if(arrayInt[i]> max) {
                 max=arrayInt[i];
             }
         }
 
-        System.out.println(max);
+        for(int i = 0; i < numberCount; i++){
+            sum += arrayInt[i] / max * 100;
+        }
 
-
-
+        System.out.println(sum / numberCount);
     }
-
-
-
 }

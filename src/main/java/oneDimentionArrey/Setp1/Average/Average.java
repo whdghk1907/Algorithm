94826b949b6c53e7cd64ef45e5022a344b70c820
CHOIYEJUN <dpwns108@gmail.com>
2023-05-24 19:20:14 +0900

    feat($Step1.Average): 백준1546번 문제 풀이
    구분 : 1차원 배열
    메모리 : 풀지 못함
    시간 : 풀지 못함
    
    시간복잡도 : 풀지 못함
    공긴복잡도 : 풀지 못함
    
    Closes 없음

--- src/main/java/oneDimentionArrey/Setp1/Average/Average.java
+++ src/main/java/oneDimentionArrey/Setp1/Average/Average.java
@@ -2,26 +2,30 @@ package oneDimentionArrey.Setp1.Average;
 
 import java.io.BufferedReader;
 import java.util.Arrays;
-        /*세준이는 기말고사를 망쳤다. 세준이는 점수를 조작해서 집에 가져가기로 했다.
-        일단 세준이는 자기 점수 중에 최댓값을 골랐다.
-        이 값을 M이라고 한다. 그리고 나서 모든 점수를 점수/M*100으로 고쳤다.
-        예를 들어, 세준이의 최고점이 70이고,
-        수학점수가 50이었으면 수학점수는 50/70*100이 되어 71.43점이 된다.
-        세준이의 성적을 위의 방법대로 새로 계산했을 때,
-        새로운 평균을 구하는 프로그램을 작성하시오.*/
+
 public class Average {
-    public static void main(String[] args) {
-        double math = 40;
-        double korean = 80;
-        double english = 60;
 
-        double changeMath = math / 80 * 100;
-        double changeKorean = korean / 80 * 100;
-        double changeEnglish = english / 80*100;
+    public static void main(String[] args) throws Exception {
 
-        double[] exScore = {changeMath, changeKorean, changeEnglish};
+        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String arr[] = br.readLine().split(" ");
+        System.out.println(arr);
+        double arrayInt[] = new double[N];
+        double max = 0;
+        double sum = 0;
 
-        System.out.println(Arrays.toString(exScore));
+        for(int i=0; i<N; i++){
+            arrayInt[i] = Integer.parseInt(arr[i]);
+            if(arrayInt[i]> max) {
+                max=arrayInt[i];
+            }
+        }
+
+        System.out.println(max);
+
+
+
     }
 
 

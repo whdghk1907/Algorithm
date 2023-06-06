1263e752d3b98882f27ee95d3147ba8ef145a153
CHOIYEJUN <dpwns108@gmail.com>
2023-06-06 16:33:05 +0900

    feat($Step1.Stack): 백준10773번 문제 풀이
    구분 : 스택(제로)
    
    메모리 : 문제풀이 실패
    시간 : 문제풀이 실패
    
    시간복잡도 : 문제풀이 실패
    공긴복잡도 : 문제풀이 실패
    
    Closes 문제풀이 실패

---
+++ src/main/java/stack/Step2/Zero.java
@@ -0,0 +1,42 @@
+package stack.Step2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Zero {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder stringBuilder = new StringBuilder();
+
+        int OrderNumber = Integer.parseInt(bufferedReader.readLine());
+
+        int[] stack = new int[OrderNumber];
+        int allSum = 0;
+        while (OrderNumber--> 0) {
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
+            int number = Integer.parseInt(stringTokenizer.nextToken());
+            if(number == 0) {
+                //stack[--OrderNumber] = 0;
+                stack[OrderNumber - 1] = 0;
+            } else {
+                stack[OrderNumber] = number;
+
+            }
+            System.out.println(stack[OrderNumber]);
+        }
+        for(int i = 0; i < stack.length; i++) {
+            allSum += stack[i];
+        }
+
+        System.out.println("정답은 : "+allSum);
+
+
+
+
+
+
+    }
+}

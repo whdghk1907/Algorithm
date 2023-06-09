6ca0842c2d60b79405bc953fcd4977dab761606a
whdghk1907 <whdghk1907@naver.com>
2023-06-09 19:53:32 +0900

    test($StackTest.java): 백준 10828번 문제의 테스트 코드 작성
    - size(), pop(), push(), top(), empty()
    메소드 들에 대하여 TDD 진행 및 구현

--- src/test/java/testStep19/Stack/StackTest.java
+++ src/test/java/testStep19/Stack/StackTest.java
@@ -4,17 +4,15 @@ import Step19.Stack.Stack;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StackTest {
     Stack stack;
 
     @BeforeEach
     void setStack() {
-        stack = new Stack(1);
+        stack = new Stack(3);
     }
-
 
     @Test
     void push() {
@@ -25,18 +23,61 @@ public class StackTest {
         stack.push(number);
 
         //then
-        assertArrayEquals(Stack.intArr, new int[]{number});
+        assertArrayEquals( new int[]{number, 0,0} , stack.getIntArr());
     }
 
     @Test
     void pop() {
         //given
-
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
 
         //when
-        stack.pop();
+        int result = stack.pop();
 
         //then
-        assertArrayEquals(Stack.intArr, new int[]{2,3});
+        assertAll(
+            () -> assertEquals(3, result),
+            () -> assertArrayEquals(new int[]{1,2,0}, stack.getIntArr())
+        );
+    }
+
+    @Test
+    void size() {
+        //given
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        // when
+        int result = stack.size();
+
+        //then
+        assertEquals(3, result);
+    }
+
+    @Test
+    void empty() {
+        //given
+        stack.push(1);
+
+        //when
+        int result = stack.empty();
+
+        //then
+        assertEquals(0, result);
+    }
+
+    @Test
+    void top() {
+        //given
+        stack.push(1);
+
+        //when
+        int result = stack.top();
+
+        //then
+        assertEquals(1, result);
     }
 }

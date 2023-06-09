9ee336f1303fef5e9aaa22354309b80251c73dc7
whdghk1907 <whdghk1907@naver.com>
2023-06-09 19:52:14 +0900

    refactor($Stack.java): 백준 10828번 풀이
    구분 : 스택
    
    메모리 : 35588KB
    시간 : 428ms
    
    시간복잡도 :  O(orderNumber)
    공간복잡도 :  O(size)
    
    BREAKING CHANGE: TDD, 자바 스타일 가이드 준수하여 리팩토링
    
    Closes #2
    Closes #3
    Closes #4

--- src/main/java/Step19/Stack/Stack.java
+++ src/main/java/Step19/Stack/Stack.java
@@ -6,25 +6,52 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Stack {
-    public static  int[] intArr;
+    public static int[] stackArr;
     public static int index = 0;
 
     public Stack(int size) {
-        intArr = new int[size];
+        stackArr = new int[size];
+        index = 0;
     }
 
     public void push(int number) {
-        intArr[index++] = number;
+        stackArr[index] = number;
+        index++;
     }
+
     public int pop() {
-        if(index != 0) {
-            int arrTop = intArr[0];
-
-
-            index--;
-            return arrTop;
+        if (index == 0) {
+            return -1;
         }
-        return -1;
+
+        int arrTop = stackArr[index - 1];
+        stackArr[index - 1] = 0;
+        index--;
+
+        return arrTop;
+    }
+
+    public int top() {
+        if (index == 0) {
+            return -1;
+        }
+        return stackArr[index];
+    }
+
+
+    public int size() {
+        return index;
+    }
+
+    public int empty() {
+        if (index == 0) {
+            return -1;
+        }
+        return 0;
+    }
+
+    public int[] getIntArr() {
+        return stackArr;
     }
 
     public static void main(String[] args) throws IOException {
@@ -36,53 +63,35 @@ public class Stack {
         StringBuilder stringBuilder = new StringBuilder();
         StringTokenizer stringTokenizer;
 
-
-        // 이거 설마 복붙이나 ChatGPT 아니죠?
-        while(orderNumber --> 0) {
-
+        while (orderNumber-- > 0) {
             stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
 
             String commandWord = stringTokenizer.nextToken();
-            int nextNumber = Integer.parseInt(stringTokenizer.nextToken());
 
-            if(commandWord.equals("push")) {
+            if (commandWord.equals("push")) {
+                int nextNumber = Integer.parseInt(stringTokenizer.nextToken());
+
                 stack.push(nextNumber);
             }
 
-            // switch 문 사용하는거 아님미다...
-            switch (stringTokenizer.nextToken()) {
-                case "push":
-                    intArr[index++] = nextNumber;
-                    break;
-                case "pop":
-                    if(index == 0) {
-                        stringBuilder.append("-1").append('\n');
-                    } else {
-                        stringBuilder.append(intArr[index - 1]).append('\n');
-                        index--;
-                    }
-                    break;
-                case "size":
-                    stringBuilder.append(index).append('\n');
-                    break;
-                case "empty":
-                    if(index == 0) {
-                        stringBuilder.append("1").append('\n');
-                    } else {
-                        stringBuilder.append("0").append('\n');
-                    }
-                    break;
-                case "top":
-                    if(index == 0) {
-                        stringBuilder.append("-1").append('\n');
-                    } else {
-                        stringBuilder.append(intArr[index - 1]).append('\n');
-                    }
-                    break;
+            if (commandWord.equals("pop")) {
+                stringBuilder.append(stack.pop()).append('\n');
+            }
+
+            if (commandWord.equals("size")) {
+                stringBuilder.append(stack.size()).append('\n');
+            }
+
+            if (commandWord.equals("empty")) {
+                stringBuilder.append(stack.empty()).append('\n');
+            }
+
+            if (commandWord.equals("top")) {
+                stringBuilder.append(stack.top()).append('\n');
             }
         }
 
-        }
-
+        System.out.println(stringBuilder);
+    }
 }
 

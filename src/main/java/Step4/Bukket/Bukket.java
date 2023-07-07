df7aec465253ab5bc2f023048ac6b6911fbad449
CHOIYEJUN <dpwns108@gmail.com>
2023-07-07 19:22:18 +0900

    feat($:Stap6.Falindrom): 팰린드롬 문제풀이
    팰린드롬 문제풀어서 커밋
    
    메모리 : 17692kb
    시간 : 204ms
    
    시간복잡도 : O(1)
    공간복잡도 : O(n)
    
    Closes 없음

--- src/main/java/Step4/Bukket/Bukket.java
+++ src/main/java/Step4/Bukket/Bukket.java
@@ -17,35 +17,6 @@ public class Bukket {
         int m = Integer.parseInt(st.nextToken());
         int a, b;
 
-//        // n 에 대한 배열 생성
-//        int[] arr = new int[n];
-//        for(int i = 0; i < n; i++) {
-//            sb.append(i);
-//        }
 
-//        for(int i = 0; i < m; i++) {
-//            st = new StringTokenizer(bufferReader.readLine());
-//            a = Integer.parseInt(st.nextToken());
-//            b = Integer.parseInt(st.nextToken());
-//
-//            // 이부분 생각 못했던 부분 찾아보고 앎
-//            // 문제에 대한 이해가 정확하게 되지 않음
-//            // 설명좀 써줘요 ㅠㅠ
-//            sb.replace(a-1, b, new StringBuffer(sb.substring(a-1, b)).reverse().toString());
-//        }
-//
-//        System.out.println(sb);
-//
-//        char[] charArray = sb.toString().toCharArray();
-//
-//        for(int i = 0; i < n - 1; i++) {
-//            bufferWriter.write(charArray[i] + " ");
-//        }
-//
-//        bufferWriter.write(charArray[n-1] + "\n");
-//
-//        bufferWriter.close();
-//        bufferReader.close();
-//
     }
 }

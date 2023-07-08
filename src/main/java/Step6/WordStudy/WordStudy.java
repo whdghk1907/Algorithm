ad4390ce4788b142405ea03ada4082615aa639b0
CHOIYEJUN <dpwns108@gmail.com>
2023-07-08 16:26:41 +0900

    feat($:Stap6.WordStudy): 백준 1157번 단어연습 문제풀이
    팰린드롬 문제풀어서 커밋
    
    메모리 : 36228kb
    시간 : 548ms
    
    시간복잡도 : O(1)
    공간복잡도 : O(n)
    
    Closes 없음

---
+++ src/main/java/Step6/WordStudy/WordStudy.java
@@ -0,0 +1,50 @@
+package Step6.WordStudy;
+
+import java.util.Scanner;
+
+public class WordStudy {
+
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine().toUpperCase();
+
+        int[] alphabet = new int[26];
+        int max = 0;
+        int wordLength = input.length();
+        String[] result = new String[wordLength];
+        int maxAlphabet = 0;
+        int maxIndex = 0;
+        String maxAlphabetString = "";
+
+
+        for (int i = 0; i < wordLength; i++) {
+
+            int index = input.charAt(i) - 65;
+
+            alphabet[index]++;
+            if (max < alphabet[index]) {
+                max = alphabet[index];
+            }
+        }
+
+        for(int i = 0; i < alphabet.length; i++ ) {
+
+            if(alphabet[i] == max) {
+                maxAlphabet = i;
+                maxIndex++;
+                maxAlphabetString = String.valueOf((char)(maxAlphabet + 65));
+            }
+        }
+
+        if (maxIndex > 1) {
+            System.out.println("?");
+            return;
+        }else {
+            System.out.println(maxAlphabetString);
+            return;
+        }
+
+
+
+    }
+}

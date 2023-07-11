04f7797b40e9e25dfdec1d91f963325fc97bf495
CHOIYEJUN <dpwns108@gmail.com>
2023-07-11 13:59:14 +0900

    feat($:Stap6.GroupWordCheck): 백준 1316번 그룹단어체크 문제풀이
    그룹단어체크 문제풀어서 커밋
    
    메모리 : 17680 kb
    시간 : 236 ms
    
    시간복잡도 : O(n^2)
    공간복잡도 : O(1)
    
    Closes 없음

--- src/main/java/Step6/CroatianAlphabet/CroatianAlphabet.java
+++ src/main/java/Step6/CroatianAlphabet/CroatianAlphabet.java
@@ -7,12 +7,12 @@ public class CroatianAlphabet {
     public static void main (String[] args){
         Scanner scanner = new Scanner(System.in);
         input = scanner.nextLine();
-        //크로아티아 알파벳의 종류 배열로 담음
+
         String[] CroatianAlphbet = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
 
 
         for (int i = 0; i < CroatianAlphbet.length; i++){
-            // 크로아티아 알파벳이 input에 포함되어 있으면 !로 바꿈
+
             if(input.contains(CroatianAlphbet[i])){
                 input = input.replace(CroatianAlphbet[i], "!");
             }

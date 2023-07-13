58e031bed3fee90fe1dd084aaa6170a5b8104788
CHOIYEJUN <dpwns108@gmail.com>
2023-07-13 18:03:50 +0900

    feat($:Stap7.MatrixAdd): 백준 2738번 행렬덧셈 문제풀이
     행렬덧셈 문제풀어서 커밋
    
    메모리 : 34884 kb
    시간 : 444 ms
    
    시간복잡도 : O(row * column)
    공간복잡도 : O(row * column)
    
    Closes : String matrixValue 썻더니 메모리초과로 틀림 StringBuilder 사용하여 품

---
+++ src/main/java/Step7/MatrixAdd/MatrixAdd.java
@@ -0,0 +1,35 @@
+package Step7.MatrixAdd;
+
+import java.util.Scanner;
+public class MatrixAdd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int row = scanner.nextInt();
+        int column = scanner.nextInt();
+        int[][] matrix1 = new int[row][column];
+        int[][] matrix2 = new int[row][column];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                matrix1[i][j] = scanner.nextInt();
+            }
+        }
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                matrix2[i][j] = scanner.nextInt();
+            }
+        }
+        // String 쓰니까 메모리 부족으로 오답됨. StringBuilder 사용해야함.
+        //String matrixValue = "";
+        StringBuilder matrixValue = new StringBuilder();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                matrixValue.append(matrix1[i][j] + matrix2[i][j]).append(" ");
+            }
+            matrixValue.append("\n");
+        }
+
+        System.out.println(matrixValue.toString());
+    }
+}

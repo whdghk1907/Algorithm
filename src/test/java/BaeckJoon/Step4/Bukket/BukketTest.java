9141af7cefa699c7cf0705734926c0612ed14280
whdghk1907 <whdghk1907@naver.com>
2023-07-21 15:13:02 +0900

    refactor($BaecJoon): JavaStyleGuide 브랜치 정리
    JavaStyleGuide 미적용 문제들 삭제

--- src/test/java/BaeckJoon/Step4/Bukket/BukketTest.java
+++
@@ -1,8 +0,0 @@
-package BaeckJoon.Step4.Bukket;
-
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class BukketTest {
-
-}
